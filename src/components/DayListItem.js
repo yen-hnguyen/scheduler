b9b34165232c8f0509413ababa0cd30d556bc8df
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-22 00:04:43 -0500

    test DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,12 +9,22 @@ export default function DayListItem(props) {
     'day-list__item--full': props.spots === 0,
   });
 
+  const formatSpots = (spot) => {
+    if (spot === 0) {
+      return <h3 className='text--light'>no spots remaining</h3>;
+    } else if (spot === 1) {
+      return <h3 className='text--light'>{spot} spot remaining</h3>;
+    } else {
+      return <h3 className='text--light'>{spot} spots remaining</h3>;
+    }
+  };
+
   return (
     <li className={dayClass}>
       <h2 onClick={() => props.setDay(props.name)} className='text--regular'>
         {props.name}
       </h2>
-      <h3 className='text--light'>{props.spots} spots remaining</h3>
+      {formatSpots(props.spots)}
     </li>
   );
 }
