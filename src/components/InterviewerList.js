5dbce5bb973d0de577870fa7f9fdfd1718fc3327
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-23 01:17:07 -0500

    build interviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import InterviewerListItem from './InterviewerListItem';
+
+import 'components/InterviewerList.scss';
+
+export default function InterviewerList(props) {
+  const interviewerListArr = props.interviewers.map((obj) => {
+    return (
+      <InterviewerListItem
+        key={obj.id}
+        avatar={obj.avatar}
+        name={obj.name}
+        selected={obj.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(obj.id)}
+      />
+    );
+  });
+
+  return (
+    <section className='interviewers'>
+      <h4 className='interviewers__header text--light'>Interviewer</h4>
+      <ul className='interviewers__list'>{interviewerListArr}</ul>
+    </section>
+  );
+}
