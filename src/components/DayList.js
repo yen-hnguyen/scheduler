0e0acdb2704c4b1f5ff54986c223b9b1140e7be7
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-22 23:25:20 -0500

    build DayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import DayListItem from './DayListItem';
+
+export default function DayList(props) {
+  const dayListArr = props.days.map((obj) => {
+    return (
+      <DayListItem
+        key={obj.id}
+        name={obj.name}
+        spots={obj.spots}
+        selected={obj.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+
+  return <ul>{dayListArr}</ul>;
+}
