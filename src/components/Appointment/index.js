9b3abca23f72ad7dd5f47eb9b6012056e70c49b8
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-25 01:13:07 -0500

    display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 import 'components/Appointment/styles.scss';
 
 export default function Appointment(props) {
-  return <article className='appointment'>{props.time}</article>;
+  return (
+    <article className='appointment'>
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
