5224dbc7ce15b550d248d67592d7ec6ef47f2b29
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-25 00:51:16 -0500

    build the Form component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
 import Error from 'components/Appointment/Error';
+import Form from 'components/Appointment/Form';
 
 storiesOf('Button', module)
   .addParameters({
@@ -162,5 +163,21 @@ storiesOf('Apppointment', module)
     <Error
       message={'Could not delete appointment'}
       onClose={action('onClose')}
+    />
+  ))
+  .add('Create', () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Edit', () => (
+    <Form
+      student={'Yen Nguyen'}
+      interviewer={1}
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
     />
   ));
