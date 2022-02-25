217ddea1ff70cb67a5e507b5ba4f786566c876ed
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-24 23:48:25 -0500

    create & setup Storybook for Error component

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
 
 storiesOf('Button', module)
   .addParameters({
@@ -156,4 +157,10 @@ storiesOf('Apppointment', module)
       onCancel={action('onCancel')}
     />
   ))
-  .add('Status', () => <Status message={'Deleting'} />);
+  .add('Status', () => <Status message={'Deleting'} />)
+  .add('Error', () => (
+    <Error
+      message={'Could not delete appointment'}
+      onClose={action('onClose')}
+    />
+  ));
