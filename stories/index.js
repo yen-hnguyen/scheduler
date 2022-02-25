4d1aaa37eae1eda380c6cf5145c9a0b60a53d8f4
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-02-24 23:34:12 -0500

    create Show component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/index.js';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
 
 storiesOf('Button', module)
   .addParameters({
@@ -137,4 +138,12 @@ storiesOf('Apppointment', module)
   .add('Appointment', () => <Appointment />)
   .add('Appointment with Time', () => <Appointment time={'12pm'} />)
   .add('Header', () => <Header time={'12pm'} />)
-  .add('Empty', () => <Empty onAdd={action('onAdd')} />);
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show
+      student={'Lydia Miller-Jones'}
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ));
