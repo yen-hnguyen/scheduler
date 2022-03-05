d4355f4bfe2c9c2fdf4d6582522339cc6384a6fe
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-03-05 16:24:43 -0500

    fix code to not directly mute the state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,13 +2,16 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState([initial]);
 
   function transition(newMode, replace = false) {
     if (replace === false) {
       setHistory((prev) => [...prev, mode]);
+      setMode(newMode);
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+      setMode(newMode);
     }
-    setMode(newMode);
   }
 
   function back() {
